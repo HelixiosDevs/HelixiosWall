9978332b749fa2189cc00d2ebc532f34ae058368
Adityachaudhary304 <aditya.ch.304@gmail.com>
2021-04-28 11:39:37 +0530

    First Commit

---
+++ app/src/main/java/com/helixios/helixioswall/MainActivity.java
@@ -0,0 +1,46 @@
+package com.helixios.helixioswall;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.transition.ChangeBounds;
+import android.transition.Fade;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.helixios.helixioswall.ui.main.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView logo = (ImageView) findViewById(R.id.logo_1);
+
+        Fade fade = new Fade();
+        View decor = getWindow().getDecorView();
+        fade.excludeTarget(decor.findViewById(R.id.action_bar_container), true);
+        fade.excludeTarget(android.R.id.statusBarBackground, true);
+        fade.excludeTarget(android.R.id.navigationBarBackground, true);
+        fade.setDuration(1500);
+        getWindow().setEnterTransition(fade);
+        getWindow().setExitTransition(fade);
+        getWindow().setSharedElementEnterTransition(new ChangeBounds().setDuration(500));
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        final int[] TAB_ICONS = new int[]{ R.drawable.home, R.drawable.category, R.drawable.heart,R.drawable.about};
+        for(int i=0; i<4; i++) {
+            TabLayout.Tab tab = tabs.getTabAt(i);
+            tab.setIcon(TAB_ICONS[i]);
+        }
+    }
+}
---
+++ app/src/main/java/com/helixios/helixioswall/adapters/RecyclerViewAdapter.java
@@ -0,0 +1,40 @@
+package com.helixios.helixioswall.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder> {
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return if (hasExtraRow()) {
+            super.getItemCount() + 1
+        } else {
+            super.getItemCount()
+        };
+    }
+
+    private boolean hasExtraRow() {
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
